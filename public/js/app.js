9569ab7b080fc96f1b4bb805c9154ba8ab3b8f6b
shaexintran <shaexin@icloud.com>
2025-06-04 22:03:22 -0700

    5 day forecast now converts to metric

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,15 @@
 const metricToggle = document.querySelector('#unitToggle');
-const windKph = document.querySelector('#windKph');
-const windMph = document.querySelector('#windMph');
-const tempC = document.querySelector('#tempC');
-const tempF = document.querySelector('#tempF');
-
-const metric = [tempC, windKph];
-const imperial = [tempF, windMph];
+const imperialUnits = document.querySelectorAll('.imperial');
+const metricUnits = document.querySelectorAll('.metric');
 
 metricToggle.checked = false;
 
 metricToggle.addEventListener('change', () => {
     if (metricToggle.checked) {
-        metric.forEach(m => m.classList.remove('hidden'));
-        imperial.forEach(i => i.classList.add('hidden'));
+        metricUnits.forEach(m => m.classList.remove('hidden'));
+        imperialUnits.forEach(i => i.classList.add('hidden'));
     } else {
-        metric.forEach(m => m.classList.add('hidden'));
-        imperial.forEach(i => i.classList.remove('hidden'));
+        metricUnits.forEach(m => m.classList.add('hidden'));
+        imperialUnits.forEach(i => i.classList.remove('hidden'));
     }
 });
